a44a2498bd1b9b5834fc44065f35a2a3f347ea2f
Felipe Dominguez <fdominguezc@uc.cl>
2021-08-11 17:58:05 -0400

    feat(models): add meter_count model

---
+++ app/admin/meters_counts.rb
@@ -0,0 +1,31 @@
+ActiveAdmin.register MeterCount do
+  belongs_to :billing_period
+
+  show do
+    attributes_table do
+      row :id
+      row :identifier
+      row :billing_period
+      row :meter
+      row :count
+      row :updated_at
+      row :created_at
+    end
+
+    panel I18n.t('activerecord.models.meter_event', count: 2) do
+      table_for resource.meter_events do
+        column(:id) do |meter_event|
+          link_to(meter_event.id,
+                  admin_organization_meter_events_path(organization_id: resource.id,
+                                                       meter_event_id: meter_event.id))
+        end
+        column :identifier
+        column :name
+        column :customer
+        column :amount
+        column :action
+        column :created_at
+      end
+    end
+  end
+end
