7fba6a41e8f1295916fdf440505c446ae7b1c6a0
Juan Ignacio Márquez <ignaciomarquezc@gmail.com>
2021-08-11 16:12:41 -0400

    fix(api): fix serializer's namespaces

--- app/controllers/api/internal/v1/base_controller.rb
+++ app/controllers/api/internal/v1/base_controller.rb
@@ -2,7 +2,7 @@ class Api::Internal::V1::BaseController < Api::BaseController
   skip_before_action :verify_authenticity_token
 
   before_action do
-    self.namespace_for_serializer = ::Api::V1
+    self.namespace_for_serializer = ::Api::Internal::V1
   end
 
   respond_to :json
