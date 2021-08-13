432e7338b7238758b2092035cdf1b04fd9a26615
Juan Ignacio Márquez <ignaciomarquezc@gmail.com>
2021-08-13 10:43:19 -0400

    feat(plutto): add plutto-modal

---
+++ app/javascript/src/components/plutto-modal.vue
@@ -0,0 +1,68 @@
+<template>
+  <Transition name="fade">
+    <div
+      v-if="showing"
+      class="fixed inset-0 z-10 flex items-center justify-center w-full h-screen bg-smoke-800"
+      @click.self="close"
+    >
+      <div class="relative flex w-full max-w-2xl max-h-screen pt-12 bg-white rounded-lg shadow-lg">
+        <button
+          aria-label="close"
+          class="absolute top-0 right-0 m-3 text-xl text-gray-500"
+          @click.prevent="close"
+        >
+          <span class="material-icons w-6 h-6 text-gray-500 fill-current hover:text-gray-600">close</span>
+        </button>
+        <div class="w-full max-h-screen overflow-auto">
+          <slot />
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<script>
+const ESCAPE_KEY = 27;
+
+export default {
+  props: {
+    showing: {
+      required: true,
+      type: Boolean,
+    },
+  },
+  watch: {
+    showing(value) {
+      if (value) {
+        return document.querySelector('body').classList.add('overflow-hidden');
+      }
+
+      return document.querySelector('body').classList.remove('overflow-hidden');
+    },
+  },
+  methods: {
+    close() {
+      this.$emit('close');
+    },
+  },
+  mounted() {
+    document.addEventListener('keydown', e => {
+      if (e.keyCode === ESCAPE_KEY) {
+        this.close();
+      }
+    });
+  },
+};
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all .3s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
