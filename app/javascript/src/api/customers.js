1c3bc92d6eb5f1242d625d5ab374a8e2ec20222f
Sebastian Margozzini <smargozzini1@gmail.com>
2021-08-13 11:55:17 -0400

    feat(customers): add customers api client

---
+++ app/javascript/src/api/customers.js
@@ -0,0 +1,27 @@
+import { camelizeKeys, decamelizeKeys } from 'humps';
+import authedAxios from '../helpers/authed-http';
+
+export function getCustomers() {
+  return authedAxios.get('/api/internal/v1/customers/')
+    .then((res) => camelizeKeys(res.data));
+}
+
+export function getCustomer(id) {
+  return authedAxios.get(`/api/internal/v1/customers/${id}`)
+    .then((res) => camelizeKeys(res.data));
+}
+
+export function create(customer) {
+  return authedAxios.post('/api/internal/v1/customers/', decamelizeKeys({ customer }))
+    .then((res) => camelizeKeys(res.data));
+}
+
+export function update(id, customer) {
+  return authedAxios.put(`/api/internal/v1/customers/${id}`, decamelizeKeys(customer))
+    .then((res) => camelizeKeys(res.data));
+}
+
+export function destroy(id) {
+  return authedAxios.delete(`/api/internal/v1/customers/${id}`)
+    .then((res) => camelizeKeys(res));
+}
