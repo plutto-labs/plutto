ec1f3b935288526546fae20910c15215e5ce7cdb
Sebastian Margozzini <smargozzini1@gmail.com>
2021-08-16 10:06:08 -0400

    feat(meters): add meters api client

---
+++ app/javascript/src/api/meters.js
@@ -0,0 +1,27 @@
+import { camelizeKeys, decamelizeKeys } from 'humps';
+import authedAxios from '../helpers/authed-http';
+
+export function getMeters() {
+  return authedAxios.get('/api/internal/v1/meters/')
+    .then((res) => camelizeKeys(res.data));
+}
+
+export function getMeter(id) {
+  return authedAxios.get(`/api/internal/v1/meters/${id}`)
+    .then((res) => camelizeKeys(res.data));
+}
+
+export function create(meter) {
+  return authedAxios.post('/api/internal/v1/meters/', decamelizeKeys({ meter }))
+    .then((res) => camelizeKeys(res.data));
+}
+
+export function update(id, meter) {
+  return authedAxios.put(`/api/internal/v1/meters/${id}`, decamelizeKeys(meter))
+    .then((res) => camelizeKeys(res.data));
+}
+
+export function destroy(id) {
+  return authedAxios.delete(`/api/internal/v1/meters/${id}`)
+    .then((res) => camelizeKeys(res));
+}
