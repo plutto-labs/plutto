6c967c1b52d06dcf1036b699a2ac5a232924db7d
Felipe Dominguez <fdominguezc@uc.cl>
2021-08-16 13:15:03 -0400

    test(helpers): add TiersFactoryHelper

--- spec/factories/price_logic/tiered.rb
+++ spec/factories/price_logic/tiered.rb
@@ -1,12 +1,8 @@
+require_relative '../../support/tiers_factory_helper'
+
 FactoryBot.define do
-  factory :tiered, class: 'PriceLogic::Tiered' do
-    trait(:with_tiers) do
-      after(:create) do |tier|
-        create(:tier, tierable: tier, lower_limit: 0, upper_limit: 10, price_cents: 0,
-                      price_currency: 'USD')
-        create(:tier, tierable: tier, lower_limit: 11, upper_limit: 20, price_cents: 0,
-                      price_currency: 'USD')
-      end
-    end
+  extend TiersFactoryHelper
+
+  factory :price_logic_tiered, class: 'PriceLogic::Tiered' do
   end
 end
