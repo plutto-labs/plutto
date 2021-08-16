66168d0c25a3b0bacbe83ccf955c84ca6e720a89
Sebastian Margozzini <smargozzini1@gmail.com>
2021-08-16 11:07:48 -0400

    feat(meters): add meters table

--- app/javascript/src/router/meters.vue
+++ app/javascript/src/router/meters.vue
@@ -4,14 +4,59 @@
       title="Meters"
       button-text="Add Meter"
     />
-    <div class="px-6 mt-6" />
+    <div class="px-6 mt-6">
+      <PluttoTable
+        class="mt-6"
+        :headers="headers"
+        :rows="meters"
+        :loading="loading"
+        @delete-clicked="destroyMeter"
+      />
+    </div>
   </main>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+import PluttoTable from '@/components/plutto-table';
 import PluttoHeader from '../components/plutto-header';
 
 export default {
-  components: { PluttoHeader },
+  components: { PluttoHeader, PluttoTable },
+  data() {
+    return {
+      headers: [
+        {
+          title: 'info',
+          type: 'twoLinesText',
+          bigText: 'name',
+          smallText: 'meterType',
+        },
+        {
+          title: 'identifier',
+          type: 'oneLineText',
+        },
+        {
+          title: 'none',
+          type: 'action',
+          action: 'delete',
+        },
+      ],
+    };
+  },
+  computed: {
+    ...mapState({
+      loading: state => state.meters.loading,
+      meters: state => state.meters.meters,
+    }),
+  },
+  async mounted() {
+    await this.$store.dispatch('GET_METERS');
+  },
+  methods: {
+    destroyMeter(meter) {
+      this.$store.dispatch('DESTROY_METER', meter);
+    },
+  },
 };
 </script>
