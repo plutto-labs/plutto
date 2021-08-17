19262571a85bb8f8c350f9738a16ccecf3e59f37
Felipe Dominguez <fdominguezc@uc.cl>
2021-08-17 11:10:26 -0400

    fix(models): add missing annotations

--- app/models/price_logic/stair_step.rb
+++ app/models/price_logic/stair_step.rb
@@ -18,3 +18,15 @@ class PriceLogic::StairStep < PriceLogic
     tiers.last
   end
 end
+
+# == Schema Information
+#
+# Table name: price_logics
+#
+#  id             :bigint(8)        not null, primary key
+#  type           :string           not null
+#  price_cents    :bigint(8)        default(0), not null
+#  price_currency :string           default("USD"), not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
