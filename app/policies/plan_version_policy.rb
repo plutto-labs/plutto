ad269bf680386bb7f347dae56727796483cdf5e0
Juan Ignacio Márquez <ignaciomarquezc@gmail.com>
2021-08-17 18:25:08 -0400

    refactor(plan_version): adapt serializer to use OrganizationRelatesPolicy

--- app/policies/plan_version_policy.rb
+++ app/policies/plan_version_policy.rb
@@ -1,24 +1,7 @@
-class PlanVersionPolicy < ApplicationPolicy
-  def initialize(current_user, record)
-    @user = current_user
-    @record = record
-  end
-
-  def create?
-    plan_from_company?
-  end
-
-  def update?
-    create?
-  end
-
-  def destroy?
-    create?
-  end
-
+class PlanVersionPolicy < OrganizationRelatedPolicy
   private
 
-  def plan_from_company?
-    @user.organization_id == @record.plan.organization_id
+  def organization_id
+    @record.plan.organization_id
   end
 end
