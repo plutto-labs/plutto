47173c0858e18ac66995b6ac0ab5ac9cf777fb13
Sebastian Margozzini <smargozzini1@gmail.com>
2021-08-18 16:27:09 -0400

    feat(settings): add settings to sidebar

--- app/javascript/src/components/side-bar.vue
+++ app/javascript/src/components/side-bar.vue
@@ -82,17 +82,30 @@
                   />
                   {{ item.label }}
                 </router-link>
+                <router-link
+                  :to="settingsItem.path"
+                  :class="[
+                    $route.path === settingsItem.path ?
+                      'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
+                  ]"
+                >
+                  <component
+                    :is="settingsItem.icon"
+                    :class="[$route.path === settingsItem.path ?
+                      'text-primary' :
+                      'text-primary-200 group-hover:text-primary-100', 'mr-3 flex-shrink-0 h-6 w-6']"
+                    aria-hidden="true"
+                  />
+                  {{ settingsItem.label }}
+                </router-link>
               </nav>
             </div>
             <div class="flex flex-shrink-0 p-4 bg-gray-700">
               <div class="flex items-center">
-                <div>
-                  <img
-                    class="inline-block w-10 h-10 rounded-full"
-                    src="https://s3.amazonaws.com/nest.platan.us/team_members/pictures/000/000/013/square/ham.jpg?1489623587"
-                    alt=""
-                  >
-                </div>
+                <p class="inline-block p-1 text-xl text-center uppercase rounded-full h-9 w-9 bg-primary">
+                  {{ currentUser.email.charAt(0) }}
+                </p>
                 <div class="ml-3">
                   <p class="text-base font-medium text-white">
                     {{ currentUser.email }}
@@ -117,7 +130,6 @@
     <!-- Static sidebar for desktop -->
     <div class="hidden md:flex md:flex-shrink-0">
       <div class="flex flex-col w-64">
-        <!-- Sidebar component, swap this element with another sidebar if you like -->
         <div class="flex flex-col flex-1 h-0 bg-gray-800">
           <div class="flex flex-col flex-1 py-6 overflow-y-auto">
             <div class="flex items-center flex-shrink-0 px-4 mb-6">
@@ -146,17 +158,30 @@
                 />
                 {{ item.label }}
               </router-link>
+              <router-link
+                :to="settingsItem.path"
+                :class="[
+                  $route.path === settingsItem.path ?
+                    'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
+                ]"
+              >
+                <component
+                  :is="settingsItem.icon"
+                  :class="[$route.path === settingsItem.path ?
+                    'text-primary' :
+                    'text-primary-200 group-hover:text-primary-100', 'mr-3 flex-shrink-0 h-6 w-6']"
+                  aria-hidden="true"
+                />
+                {{ settingsItem.label }}
+              </router-link>
             </nav>
           </div>
           <div class="flex flex-shrink-0 p-4 bg-gray-700">
             <div class="flex items-center">
-              <div>
-                <img
-                  class="inline-block rounded-full h-9 w-9"
-                  src="https://s3.amazonaws.com/nest.platan.us/team_members/pictures/000/000/013/square/ham.jpg?1489623587"
-                  alt=""
-                >
-              </div>
+              <p class="inline-block p-1 text-xl text-center uppercase rounded-full h-9 w-9 bg-primary">
+                {{ currentUser.email.charAt(0) }}
+              </p>
               <div class="ml-3">
                 <p class="text-sm font-medium text-white">
                   {{ currentUser.email }}
@@ -201,6 +226,7 @@ import {
   UsersIcon,
   XIcon,
   LightningBoltIcon,
+  CogIcon,
 } from '@heroicons/vue/outline';
 
 const navigation = [
@@ -210,6 +236,11 @@ const navigation = [
 ];
 
 export default {
+  data() {
+    return {
+      settingsItem: { label: 'Settings', path: '/settings', icon: CogIcon },
+    };
+  },
   components: {
     Dialog,
     DialogOverlay,
---
+++ app/javascript/src/router/settings.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <PluttoHeader
+      title="Settings"
+    />
+    <div class="px-6 mt-6" />
+  </main>
+</template>
+
+<script>
+import PluttoHeader from '@/components/plutto-header';
+
+export default {
+  components: { PluttoHeader },
+};
+</script>
