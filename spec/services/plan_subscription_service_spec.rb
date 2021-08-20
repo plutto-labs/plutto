dfb87ea26ec0f20d01c083ef7925f0cb29dcd1d6
Felipe Dominguez <fdominguezc@uc.cl>
2021-08-20 17:28:54 -0400

    feat(subscription): create PlanSubscriptionService

---
+++ spec/services/plan_subscription_service_spec.rb
@@ -0,0 +1,54 @@
+describe PlanSubscriptionService do
+  let(:plan_subscription) { create(:plan_subscription) }
+  let(:service) { described_class.new(plan_subscription: plan_subscription) }
+  let(:date) { DateTime.new(2021, 3, 20, 12, 0, 0) }
+  let!(:billing_period) do
+    create(:billing_period, plan_subscription: plan_subscription, from: date - 1.month, to: date)
+  end
+
+  before do
+    Timecop.freeze(date)
+    allow(StartBillingPeriod).to receive(:for).and_return(usd(100))
+  end
+
+  describe '#end_billing_period' do
+    before do
+      allow(BillingPeriodPriceCalculator).to receive(:for).and_return(usd(100))
+    end
+
+    it 'set the current Date as billing_date' do
+      expect { service.end_billing_period }.to change { billing_period.reload.billing_date }
+        .from(nil).to(date)
+    end
+
+    it 'gets the current billing amount' do
+      period_days = (billing_period.from - billing_period.to).to_i
+      service.end_billing_period
+
+      expect(BillingPeriodPriceCalculator).to have_received(:for).with(
+        meter_counts: billing_period.meter_counts,
+        plan_subscription: plan_subscription,
+        total_period_days: period_days,
+        days_in_periods: period_days
+      )
+    end
+
+    it 'sets the billing_amount' do
+      expect { service.end_billing_period }.to change { billing_period.reload.billing_amount }
+        .from(usd(0)).to(usd(100))
+    end
+  end
+
+  describe '#initialize_next_billing_period' do
+    it 'creates the next billing period' do
+      expect { service.end_billing_period }.to change { plan_subscription.billing_periods.count }
+        .by(1)
+    end
+
+    it 'call StartBillingPeriod command' do
+      service.end_billing_period
+      expect(StartBillingPeriod).to have_received(:for)
+        .with(billing_period: plan_subscription.billing_periods.last)
+    end
+  end
+end
