7d6cec48413c5ac7d19dcdd32654c1b45a8ade4e
Juan Ignacio Márquez <ignaciomarquezc@gmail.com>
2021-08-23 11:57:04 -0400

    feat(plan-version): add component to display plan versions

---
+++ app/javascript/src/components/plan-version.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="flex items-center justify-between">
+      <div>
+        {{ planVersion.version }}
+        <span
+          v-if="isDefaultVersion"
+          class="ml-4 plutto-tag"
+        >Default version</span>
+      </div>
+      <span
+        class="text-xl cursor-pointer plutto-icon"
+        :class="{ 'transform rotate-180': expanded }"
+        @click="expanded = !expanded"
+      >
+        expand_more
+      </span>
+    </div>
+    <template v-if="expanded">
+      <div class="w-full m-auto space-y-8 divide-y divide-gray-200 md:max-w-xl">
+        <div class="mt-8 mb-4 text-lg">
+          Price Logics
+        </div>
+        <PriceLogics
+          :model-value="planVersion.priceLogics"
+          :edit="false"
+        />
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+import PriceLogics from '@/components/price-logics';
+
+export default {
+  components: { PriceLogics },
+  props: {
+    planVersion: {
+      type: Object,
+      required: true,
+    },
+    isDefaultVersion: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      expanded: false,
+    };
+  },
+};
+</script>
