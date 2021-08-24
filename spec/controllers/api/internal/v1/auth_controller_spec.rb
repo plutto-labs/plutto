29902e3c1866007aec88f4ea0090c4e40435ee7b
Sebastian Margozzini <smargozzini1@gmail.com>
2021-08-24 13:25:19 -0400

    fix(auth): fix auth test

--- spec/controllers/api/internal/v1/auth_controller_spec.rb
+++ spec/controllers/api/internal/v1/auth_controller_spec.rb
@@ -1,8 +1,25 @@
 RSpec.describe Api::Internal::V1::AuthController, type: :controller do
   describe 'create' do
-    let!(:user) { create(:user, email: 'admin@example.com', password: '123123') }
+    let(:organization) { create(:organization) }
+    let!(:user) do
+      create(
+        :user,
+        email: 'admin@example.com',
+        password: '123123',
+        organization: organization
+      )
+    end
 
     context 'with valid parameters' do
+      before do
+        allow(Analytics).to receive(:identify).with(user_id: user.id, traits: { email: user.email })
+        allow(Analytics).to receive(:group).with(
+          user_id: user.id,
+          group_id: user.organization.id,
+          traits: { organization_name: user.organization.name }
+        )
+      end
+
       let(:valid_params) do
         {
           email: 'admin@example.com',
