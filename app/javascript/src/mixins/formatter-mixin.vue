b517df7c8c5b9b2bef8755803fcd2f635e7d3623
Juan Ignacio Márquez <ignaciomarquezc@gmail.com>
2021-08-24 15:29:45 -0400

    feat(vue): add formatter-mixin

---
+++ app/javascript/src/mixins/formatter-mixin.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+  methods: {
+    formatCurrency(val, currency = 'CLP') {
+      return new Intl.NumberFormat('pt-BR', { style: 'currency', currency }).format(val);
+    },
+  },
+};
+</script>
