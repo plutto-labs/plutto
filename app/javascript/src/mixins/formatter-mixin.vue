2f907199e824d8dfc7218cb6a71ebbdca2d3c60f
Juan Ignacio Márquez <ignaciomarquezc@gmail.com>
2021-08-27 16:21:03 -0400

    feat(currencies): move all currency definition to one place

--- app/javascript/src/mixins/formatter-mixin.vue
+++ app/javascript/src/mixins/formatter-mixin.vue
@@ -1,5 +1,13 @@
 <script>
+import currencies from '../../../../config/currencies.json';
+
 export default {
+  data() {
+    return {
+      CURRENCIES: currencies,
+      CURRENCY_KEYS: Object.keys(currencies),
+    };
+  },
   methods: {
     formatCurrency(val, currency = 'CLP') {
       return new Intl.NumberFormat('pt-BR', { style: 'currency', currency }).format(val);
