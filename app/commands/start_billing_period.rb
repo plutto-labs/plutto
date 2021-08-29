acd3b400ca7579ea55215018ca53cbb354a2dd16
Sebastian Margozzini <smargozzini1@gmail.com>
2021-08-29 15:28:05 -0400

    feat(start-billing-period): refactor commando to new data structure

--- app/commands/start_billing_period.rb
+++ app/commands/start_billing_period.rb
@@ -2,8 +2,27 @@ class StartBillingPeriod < PowerTypes::Command.new(:billing_period)
   def perform
     @billing_period.plan_version_price_logics.each do |price_logic|
       if price_logic.class.metered?
-        MeterCount.create!(billing_period: @billing_period, meter: price_logic.meter)
+        meter_count = find_or_create_meter_count(price_logic.meter)
+        billing_period_meter_data = find_or_create_billing_period_meter_date(meter_count)
+        billing_period_meter_data.update(initial_count: meter_count.count)
       end
     end
+  end
+
+  private
+
+  def customer
+    @customer ||= @billing_period.plan_subscription.customer
+  end
+
+  def find_or_create_meter_count(meter)
+    MeterCount.find_or_create_by!(customer: customer, meter: meter)
+  end
+
+  def find_or_create_billing_period_meter_date(meter_count)
+    BillingPeriodMeterData.find_or_create_by!(
+      billing_period: @billing_period,
+      meter_count: meter_count
+    )
   end
 end
--- spec/commands/start_billing_period_spec.rb
+++ spec/commands/start_billing_period_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 describe StartBillingPeriod do
+  let(:customer) { create(:customer) }
   let(:plan_version) { build(:plan_version) }
-  let(:plan_subscription) { build(:plan_subscription, plan_version: plan_version) }
+  let(:plan_subscription) do
+    build(:plan_subscription, plan_version: plan_version, customer: customer)
+  end
   let(:billing_period) { build(:billing_period, plan_subscription: plan_subscription) }
 
   def perform
@@ -11,13 +14,61 @@ describe StartBillingPeriod do
 
   describe '#perform' do
     context 'when it has metered price logics' do
-      let(:metered_price_logics) { 3 }
-      let!(:price_logics) do
-        create_list(:price_logic_per_unit, metered_price_logics, plan_version: plan_version)
+      context 'when it has not repeated meters' do
+        before do
+          meter1 = create(:meter)
+          meter2 = create(:meter)
+          meter3 = create(:meter)
+          create(:price_logic_per_unit, plan_version: plan_version, meter: meter1)
+          create(:price_logic_per_unit, plan_version: plan_version, meter: meter2)
+          create(:price_logic_per_unit, plan_version: plan_version, meter: meter3)
+        end
+
+        it { expect { perform }.to change { BillingPeriodMeterData.count }.by(3) }
+
+        context 'when it has not meter counts created' do
+          it 'creates a meter count for each one' do
+            expect { perform }.to change { MeterCount.count }.by(3)
+          end
+        end
+
+        context 'when it has price counts created' do
+          before do
+            create(:meter_count, customer: customer, meter: Meter.first)
+          end
+
+          it 'only creates necessary meter counts' do
+            expect { perform }.to change { MeterCount.count }.by(2)
+          end
+        end
       end
 
-      it 'creates a meter count for each one' do
-        expect { perform }.to change { MeterCount.count }.by(metered_price_logics)
+      context 'when it has repeated meters' do
+        before do
+          meter1 = create(:meter)
+          meter2 = create(:meter)
+          create(:price_logic_per_unit, plan_version: plan_version, meter: meter1)
+          create(:price_logic_per_unit, plan_version: plan_version, meter: meter2)
+          create(:price_logic_per_unit, plan_version: plan_version, meter: meter2)
+        end
+
+        it { expect { perform }.to change { BillingPeriodMeterData.count }.by(2) }
+
+        context 'when it has not meter counts created' do
+          it 'creates a meter count for each one' do
+            expect { perform }.to change { MeterCount.count }.by(2)
+          end
+        end
+
+        context 'when it has meter counts created' do
+          before do
+            create(:meter_count, customer: customer, meter: Meter.first)
+          end
+
+          it 'only creates necessary meter counts' do
+            expect { perform }.to change { MeterCount.count }.by(1)
+          end
+        end
       end
     end
 
