705562c5044fc3a2e23774a6d70873053302d82d
Sebastian Margozzini <smargozzini1@gmail.com>
2021-08-29 21:18:07 -0400

    feat(billing-period-price-calculator): refactor command

--- app/commands/billing_period_price_calculator.rb
+++ app/commands/billing_period_price_calculator.rb
@@ -1,26 +1,38 @@
 class BillingPeriodPriceCalculator < PowerTypes::Command.new(
-  :meter_counts, :plan_subscription, :total_period_days, :days_in_periods
+  :billing_period
 )
 
   def perform
     total_price = 0
-    @plan_subscription.plan_version_price_logics.each do |price_logic|
-      meter_count = meter_count_for_price_logic(price_logic)
-      total_price += price_logic.calculate_price(meter_count&.count)
+    plan_subscription.plan_version_price_logics.each do |price_logic|
+      billing_period_meter_data = billing_period_meter_data_for_price_logic(price_logic)
+      total_price += price_logic.calculate_price(billing_period_meter_data&.count || 0)
     end
-
     total_price * period_duration_ratio
   end
 
   private
 
-  def meter_count_for_price_logic(price_logic)
+  def billing_period_meter_data_for_price_logic(price_logic)
     return nil unless price_logic.class.metered?
 
-    @meter_counts.find { |meter_count| meter_count.meter_id == price_logic.meter_id }
+    meter_count = customer.meter_counts.find_by(meter: price_logic.meter)
+    @billing_period.billing_period_meter_datas.find_by(meter_count: meter_count)
+  end
+
+  def customer
+    @customer = plan_subscription.customer
+  end
+
+  def plan_subscription
+    @plan_subscription = @billing_period.plan_subscription
   end
 
   def period_duration_ratio
-    @days_in_periods.to_f / @total_period_days
+    total_period_days = (@billing_period.from - @billing_period.to).to_i
+    days_in_period = (@billing_period.from - @billing_period.billing_date).to_i
+    ratio = days_in_period.to_f / total_period_days
+
+    ratio > 1 ? 1 : ratio
   end
 end
--- spec/commands/billing_period_price_calculator_spec.rb
+++ spec/commands/billing_period_price_calculator_spec.rb
@@ -1,14 +1,26 @@
 require 'rails_helper'
 
 describe BillingPeriodPriceCalculator do
-  let(:meter_counts) { [] }
-  let(:days_in_periods) { 30 }
-  let(:plan_version) { build(:plan_version) }
-  let(:plan_subscription) { build(:plan_subscription, plan_version: plan_version) }
+  let(:customer) { create(:customer) }
+  let(:from) { Time.current }
+  let(:to) { Time.current + 30.days }
+  let(:billing_date) { to }
+  let(:plan_version) { create(:plan_version) }
+  let(:plan_subscription) do
+    create(:plan_subscription, plan_version: plan_version, customer: customer)
+  end
+  let(:billing_period) do
+    create(
+      :billing_period,
+      from: from,
+      to: to,
+      plan_subscription: plan_subscription,
+      billing_date: billing_date
+    )
+  end
 
   def perform
-    described_class.for(meter_counts: meter_counts, plan_subscription: plan_subscription,
-                        total_period_days: 30, days_in_periods: days_in_periods)
+    described_class.for(billing_period: billing_period)
   end
 
   describe '#perform' do
@@ -24,8 +36,17 @@ describe BillingPeriodPriceCalculator do
     end
 
     context 'when there are multiple price logic' do
-      let(:meter) { build(:meter) }
-      let!(:meter_counts) { [create(:meter_count, meter: meter, count: 250)] }
+      let(:meter) { create(:meter, meter_type: 'period_sum') }
+      let!(:meter_count) { create(:meter_count, meter: meter, count: 250, customer: customer) }
+      let!(:billing_period_meter_data) do
+        create(
+          :billing_period_meter_data,
+          initial_count: 10,
+          final_count: 260,
+          billing_period: billing_period,
+          meter_count: meter_count
+        )
+      end
 
       before do
         tiers_params = [{ price: usd(300), limit: 100 }, { price: usd(200), limit: 200 },
@@ -45,6 +66,7 @@ describe BillingPeriodPriceCalculator do
 
       context 'when billing period end before the end of period' do
         let(:days_in_periods) { 15 }
+        let(:billing_date) { Time.current + 15.days }
 
         it 'returns the correct price' do
           expected_price = (usd(100) * 250 + usd(100)) * 0.5
