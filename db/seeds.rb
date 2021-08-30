7e2dfbf4c33e198d8c8f88d224721c8d6ab147c3
Felipe Dominguez <fdominguezc@uc.cl>
2021-08-30 11:51:48 -0400

    feat(seeds): add basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,49 @@
 # Country.create(name: "Chile") # BAD
 # Country.find_or_create_by(name: "Chile") # GOOD
 #
+
+unless Rails.env.production?
+  plutto_user_mails = ['felipe@getplutto.com', 'antonia@getplutto.com',
+                       'sebastian@getplutto.com', 'ignacio@getplutto.com']
+  customers = ['donald@mars.com', 'ham@platan.us', 'teddy@apple.com']
+
+  plutto = Organization.find_or_create_by(name: 'Plutto')
+
+  plutto_user_mails.each do |email|
+    User.find_or_create_by(email: email) do |user|
+      user.password = 'password'
+      user.organization = plutto
+    end
+
+    AdminUser.find_or_create_by(email: email) do |user|
+      user.password = 'password'
+    end
+  end
+
+  meter = Meter.find_or_create_by(name: 'api calls', organization: plutto) do |meter|
+    meter.meter_type = 'history_sum'
+  end
+
+  customers.each do |customer|
+    Customer.find_or_create_by(email: customer, organization: plutto)
+  end
+
+  plan = Plan.find_or_create_by(name: 'Plutto', organization: plutto) do |plan|
+    plan.currency = 'USD'
+  end
+
+  plan_version = PlanVersion.find_or_create_by(plan: plan) do |plan_version|
+    plan_version.deployed = true
+    plan.default_version = plan_version
+  end
+
+  if plan_version
+    PriceLogic::FlatFee.find_or_create_by(plan_version: plan_version) do |flat_fee|
+      flat_fee.price = Money.new(100, plan.currency)
+    end
+
+    PriceLogic::PerUnit.find_or_create_by(plan_version: plan_version, meter: meter) do |per_unit|
+      per_unit.price = Money.new(5, plan.currency)
+    end
+  end
+end
