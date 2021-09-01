b8fe36063e0626996f487715a83d620df0237c47
Sebastian Margozzini <smargozzini1@gmail.com>
2021-09-01 15:28:23 -0400

    feat(billing-periods): rename command

--- app/commands/set_initial_data_to_billing_period.rb
+++ app/commands/set_initial_data_to_billing_period.rb
@@ -1,9 +1,9 @@
-class StartBillingPeriod < PowerTypes::Command.new(:billing_period)
+class SetInitialDataToBillingPeriod < PowerTypes::Command.new(:billing_period)
   def perform
     @billing_period.plan_version_price_logics.each do |price_logic|
       if price_logic.class.metered?
         meter_count = find_or_create_meter_count(price_logic.meter)
-        billing_period_meter_data = find_or_create_billing_period_meter_date(meter_count)
+        billing_period_meter_data = find_or_create_billing_period_meter_data(meter_count)
         billing_period_meter_data.update(initial_count: meter_count.count)
       end
     end
@@ -19,7 +19,7 @@ class StartBillingPeriod < PowerTypes::Command.new(:billing_period)
     MeterCount.find_or_create_by!(customer: customer, meter: meter)
   end
 
-  def find_or_create_billing_period_meter_date(meter_count)
+  def find_or_create_billing_period_meter_data(meter_count)
     BillingPeriodMeterData.find_or_create_by!(
       billing_period: @billing_period,
       meter_count: meter_count
