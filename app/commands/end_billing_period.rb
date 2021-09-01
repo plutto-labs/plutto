bef4bf7749ca71622c6be79494eb2b0c53c44e23
Sebastian Margozzini <smargozzini1@gmail.com>
2021-09-01 15:57:03 -0400

    feat(end-billing-period): move end billing period to command

---
+++ app/commands/end_billing_period.rb
@@ -0,0 +1,27 @@
+class EndBillingPeriod < PowerTypes::Command.new(:billing_period, start_next_period: true)
+  def perform
+    ActiveRecord::Base.with_advisory_lock("meter-events-#{plan_subscription.identifier}-lock") do
+      ActiveRecord::Base.transaction do
+        @billing_period.update(billing_date: Date.current)
+        create_invoice unless @plan_subscription.bills_at_start?
+
+        if @start_next_period
+          StartNewBillingPeriod.for(
+            plan_subscription: plan_subscription,
+            billing_period: @billing_period
+          )
+        end
+      end
+    end
+  end
+
+  private
+
+  def plan_subscription
+    @plan_subscription ||= @billing_period.plan_subscription
+  end
+
+  def create_invoice
+    CreateInvoice.for(billing_period: @billing_period)
+  end
+end
--- app/services/plan_subscription_service.rb
+++ app/services/plan_subscription_service.rb
@@ -1,16 +1,4 @@
 class PlanSubscriptionService < PowerTypes::Service.new(:plan_subscription)
-  def end_billing_period(start_new_period = true)
-    ActiveRecord::Base.with_advisory_lock("meter-events-#{@plan_subscription.identifier}-lock") do
-      ActiveRecord::Base.transaction do
-        billing_period.billing_date = Date.current
-        billing_period.billing_amount = get_billing_amount_for_period
-        billing_period.save!
-      end
-
-      initialize_next_billing_period if start_new_period
-    end
-  end
-
   def initialize_next_billing_period
     first_day = billing_period ? billing_period.to + 1.day : Date.current
 
