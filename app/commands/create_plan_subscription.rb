d9188f3841434fc007bf0008d332800b61fe8af5
Sebastian Margozzini <smargozzini1@gmail.com>
2021-09-01 18:10:42 -0400

    feat(create-plan-subscription): changes service to commands

--- app/commands/create_plan_subscription.rb
+++ app/commands/create_plan_subscription.rb
@@ -1,26 +1,20 @@
 class CreatePlanSubscription < PowerTypes::Command.new(:plan_version, :customer)
   def perform
-    new_plan_subscription = nil
     ActiveRecord::Base.transaction do
       active_plan_subscription = @customer.active_plan_subscription
       if active_plan_subscription.present?
-        plan_subscription_service(active_plan_subscription)
-          .end_billing_period(start_new_period: false)
+        EndBillingPeriod.for(
+          billing_period: active_plan_subscription.current_billing_period,
+          start_next_period: false
+        )
         active_plan_subscription.update(active: false)
       end
 
-      new_plan_subscription = create_plan_subscription!
-      plan_subscription_service(new_plan_subscription).initialize_next_billing_period
+      create_plan_subscription!
     end
-
-    new_plan_subscription
   end
 
   private
-
-  def plan_subscription_service(plan_subscription)
-    PlanSubscriptionService.new(plan_subscription: plan_subscription)
-  end
 
   def create_plan_subscription!
     PlanSubscription.create!(customer: @customer, plan_version: @plan_version, active: true)
