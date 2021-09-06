53bdf422fdcdbf323eafa9685609332b09b11935
Sebastian Margozzini <smargozzini1@gmail.com>
2021-09-06 15:20:03 -0300

    feat(subscriptios): use plutto table

--- app/javascript/src/router/subscriptions.vue
+++ app/javascript/src/router/subscriptions.vue
@@ -1,12 +1,67 @@
 <template>
-  <div class="py-12 mx-auto text-xl border rounded-lg px-36 text-primary border-primary">
-    Coming soon...
+  <div>
+    <h2 class="mb-4">
+      Subscriptions
+    </h2>
+    <div>
+      <PluttoTable
+        :headers="headers"
+        :rows="invoices"
+        :loading="loading"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+import PluttoTable from '@/components/plutto-table';
+
 export default {
+  components: { PluttoTable },
   data() {
+    return {
+      headers: [
+        {
+          title: 'customer',
+          type: 'twoLinesText',
+          bigText: 'customerEmail',
+          smallText: 'customerName',
+        },
+        {
+          title: 'state',
+          type: 'tag',
+          tags: {
+            new: 'purple',
+            posted: 'blue',
+            paid: 'green',
+            notPaid: 'red',
+            voided: 'yellow',
+          },
+        },
+        {
+          title: 'subtotal',
+          type: 'currency',
+        },
+        {
+          title: 'currency',
+          type: 'oneLineText',
+        },
+        {
+          title: 'issueDate',
+          type: 'date',
+        },
+      ],
+    };
+  },
+  computed: {
+    ...mapState({
+      loading: state => state.invoices.loading,
+      invoices: state => state.invoices.invoices,
+    }),
+  },
+  async mounted() {
+    await this.$store.dispatch('GET_INVOICES');
   },
 };
 </script>
