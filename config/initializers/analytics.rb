3e211a315ee4dba87ac2040e7b5445cc322f6814
Felipe Dominguez <fdominguezc@uc.cl>
2021-09-06 14:42:23 -0300

    fix(analytics): add correct analytics condition

--- config/initializers/analytics.rb
+++ config/initializers/analytics.rb
@@ -1,6 +1,7 @@
-SEGMENT_WRITE_KEY = ENV.fetch('REPORT_ANALYTICS', false) ? ENV.fetch('SEGMENT_RUBY_WRITE_KEY') : ''
+SEGMENT_WRITE_KEY =
+  ENV.fetch('REPORT_ANALYTICS', false) == 'true' ? ENV.fetch('SEGMENT_RUBY_WRITE_KEY') : ''
 
 Analytics = Segment::Analytics.new(
   write_key: SEGMENT_WRITE_KEY,
-  on_error: Proc.new { |_, msg| print msg }
+  on_error: Proc.new { |_, msg| logger.error(msg) }
 )
