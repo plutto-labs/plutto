9f6ad87503004512898ac3c923cd3ec635b3b00d
Juan Ignacio Márquez <ignaciomarquezc@gmail.com>
2021-09-08 18:12:07 -0300

    feat(user): add method to retrieve current user

--- app/javascript/src/api/auth.js
+++ app/javascript/src/api/auth.js
@@ -1,8 +1,13 @@
-import axios from 'axios';
 import { camelizeKeys } from 'humps';
+import axios from 'axios';
+import authedAxios from '../helpers/authed-http';
 
-export default function login(email, password) {
+export function login(email, password) {
   return axios.post('/api/internal/v1/auth.json', { auth: { email, password } })
     .then((res) => camelizeKeys(res.data));
 }
 
+export function getUser(id) {
+  return authedAxios.get(`/api/internal/v1/users/${id}`)
+    .then((res) => camelizeKeys(res.data));
+}
