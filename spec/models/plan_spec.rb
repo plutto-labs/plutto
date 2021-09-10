8b8d6d7910d3102c270ddce26a315de43d175d2b
Juan Ignacio Márquez <ignaciomarquezc@gmail.com>
2021-09-10 14:57:03 -0300

    feat(plan): validate no metered price logics for bills_at_start plan

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -3,4 +3,38 @@ RSpec.describe Plan, type: :model do
     it { is_expected.to have_many(:plan_versions) }
     it { is_expected.to belong_to(:organization) }
   end
+
+  describe 'Validations' do
+    describe '#no_metered_for_bills_at_start' do
+      let(:bills_at) { 'start' }
+      let(:plan) { build(:plan, bills_at: bills_at) }
+      let(:plan_version) { create(:plan_version, plan: plan) }
+
+      context 'when it bills at start' do
+        it 'is not valid for metered price_logic' do
+          plan_version.price_logics << create(:price_logic_stair_step)
+          expect(plan.reload).not_to be_valid
+        end
+
+        it 'is valid for not metered price_logic' do
+          plan_version.price_logics << create(:price_logic_flat_fee)
+          expect(plan.reload).to be_valid
+        end
+      end
+
+      context 'when it bills at end' do
+        let(:bills_at) { 'end' }
+
+        it 'is valid for metered price_logic' do
+          plan_version.price_logics << create(:price_logic_stair_step)
+          expect(plan.reload).to be_valid
+        end
+
+        it 'is valid for not metered price_logic' do
+          plan_version.price_logics << create(:price_logic_flat_fee)
+          expect(plan.reload).to be_valid
+        end
+      end
+    end
+  end
 end
