971e65c2240b8d0909b5bc10daef18dd4aa496fd
Sebastian Margozzini <smargozzini1@gmail.com>
2021-09-14 09:33:08 -0300

    WIP

--- config/initializers/ledgerizer.rb
+++ config/initializers/ledgerizer.rb
@@ -1,22 +1,5 @@
-# Example:
-#
-# Ledgerizer.setup do |conf|
-#   conf.tenant(:portfolio) do
-#     conf.asset :bank
-#     conf.liability :funds_to_invest
-#     conf.liability :to_invest_in_fund
-#
-#     conf.entry :user_deposit, document: :user_deposit do
-#       conf.debit account: :bank, accountable: :bank
-#       conf.credit account: :funds_to_invest, accountable: :user
-#     end
-#
-#     conf.entry :user_deposit_distribution, document: :user_deposit do
-#       conf.debit account: :funds_to_invest, accountable: :user
-#       conf.credit account: :to_invest_in_fund, accountable: :user
-#     end
-#   end
-# end
+CURRENCIES = JSON.parse(File.read(File.expand_path('../currencies.json', __dir__)))
+LEDGERIZER_CURRENCIES = CURRENCIES.keys.map &:to_sym
 
-Ledgerizer.setup do |_config|
+Ledgerizer.setup do |conf|
 end
