1d4928d07ac57983a1c4f023b6862cb3f9444385
Sebastian Margozzini <smargozzini1@gmail.com>
2021-09-16 17:55:08 -0300

    feat(customers): move customers table to all section

---
+++ app/javascript/src/router/customers/active.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    Active users
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+import PluttoTable from '@/components/plutto-table';
+
+export default {
+  components: {
+    PluttoTable,
+  },
+  data() {
+    return {
+      headers: [{
+        title: 'info',
+        type: 'twoLinesText',
+        bigText: 'name',
+        smallText: 'email',
+      }, {
+        title: 'identifier',
+        type: 'oneLineText',
+      }, {
+        title: 'id',
+        type: 'copyableLine',
+      }, {
+        title: 'planSubscription',
+        type: 'component',
+      }, {
+        title: 'none',
+        type: 'action',
+        action: 'edit',
+      }],
+    };
+  },
+};
+</script>
