9ea575366c742ec196b029c42760ae87c284679d
Juan Ignacio Márquez <ignaciomarquezc@gmail.com>
2021-09-21 18:01:54 -0300

    feat(billing-period-price-details): remove FlatFee price ratio

--- app/commands/billing_period_price_details.rb
+++ app/commands/billing_period_price_details.rb
@@ -8,7 +8,6 @@ class BillingPeriodPriceDetails < PowerTypes::Command.new(:billing_period)
       price_logic_price = price_logic.calculate_price(
         billing_period_meter_data&.count(price_logic.meter_count_method) || 0
       )
-      price_logic_price *= period_duration_ratio if !price_logic.metered?
       details[i] = details_from_price_logic(price_logic, billing_period_meter_data,
                                             price_logic_price)
 
@@ -33,21 +32,6 @@ class BillingPeriodPriceDetails < PowerTypes::Command.new(:billing_period)
 
   def plan_subscription
     @plan_subscription = @billing_period.plan_subscription
-  end
-
-  def period_duration_ratio
-    return 1 if plan_subscription.bills_at_start?
-
-    total_period_days = (
-      @billing_period.to.to_datetime - @billing_period.from.to_datetime
-    ).to_i
-    days_in_period = (
-      @billing_period.billing_date.to_datetime - @billing_period.from.to_datetime
-    ).to_i + 1
-
-    ratio = days_in_period.to_f / total_period_days
-
-    ratio > 1 ? 1 : ratio
   end
 
   def details_from_price_logic(price_logic, billing_period_meter_data, total_price)
