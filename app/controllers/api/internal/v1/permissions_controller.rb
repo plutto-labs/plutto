71f781e67e0a50bd0fdd2927ec93af7ea4665e62
Juan Ignacio Márquez <ignaciomarquezc@gmail.com>
2021-10-07 12:01:14 -0300

    feat(permission): add controller, policy and serializer

---
+++ app/controllers/api/internal/v1/permissions_controller.rb
@@ -0,0 +1,27 @@
+class Api::Internal::V1::PermissionsController < Api::Internal::V1::BaseController
+  acts_as_token_authentication_handler_for User
+  include Pundit
+
+  def index
+    respond_with(authorize(permissions))
+  end
+
+  def create
+    permission = authorize(
+      Permission.create!(permission_params.merge(organization_id: current_user.organization_id))
+    )
+    respond_with(permission)
+  end
+
+  private
+
+  def permission_params
+    params.require(:permission).permit(
+      :name, :meter_id, :meter_count_method
+    )
+  end
+
+  def permissions
+    @permissions ||= policy_scope(Permission)
+  end
+end
---
+++ app/serializers/api/internal/v1/permission_serializer.rb
@@ -0,0 +1,3 @@
+class Api::Internal::V1::PermissionSerializer < Api::BaseSerializer
+  attributes :id, :name, :meter_id
+end
