cc509108a4cc58e6b1ae45bc5b58880bea979a9f
Sebastian Margozzini <smargozzini1@gmail.com>
2021-10-13 12:54:59 -0300

    feat(tooltip): create tooltip

---
+++ app/javascript/src/components/plutto-tooltip.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    @mouseover="onHover"
+    @mouseout="show = false"
+  >
+    <slot name="trigger" />
+    <div
+      class="absolute z-10 flex flex-col mr-2"
+      v-if="show"
+    >
+      <span class="w-2 h-2 ml-4 origin-bottom-left transform rotate-45 bg-gray-600" />
+      <div class="px-3 py-1 bg-gray-600 rounded">
+        <slot name="content" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+    };
+  },
+  methods: {
+    onHover() {
+      this.show = true;
+    },
+  },
+};
+</script>
