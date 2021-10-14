a6264690b36fb095a937edcbdb317771e7d348d6
Juan Ignacio Márquez <ignaciomarquezc@gmail.com>
2021-10-14 16:24:18 -0300

    fix(errors): display 500 errors correctly

--- app/javascript/src/components/plutto-error.vue
+++ app/javascript/src/components/plutto-error.vue
@@ -4,7 +4,7 @@
       class="absolute right-0 flex items-center p-6 text-sm border rounded-l text-danger bg-danger-light top-8 border-danger-light"
       v-if="globalError && globalError.response"
     >
-      <span>{{ globalError.response.data.error.detail || globalError.response.data.error }}</span>
+      <span>{{ parsedError.detail || parsedError.message || parsedError }}</span>
       <span
         @click="$store.dispatch('CLEAR_ERROR')"
         class="ml-4 text-xl cursor-pointer plutto-icon"
@@ -21,6 +21,9 @@ export default {
     ...mapState({
       globalError: state => state.ui.error,
     }),
+    parsedError() {
+      return this.globalError.response.data.error;
+    },
   },
 };
 </script>
