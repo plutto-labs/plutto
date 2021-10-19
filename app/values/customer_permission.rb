99874fdd8cdf785025dfe3e99d4bd0d0efa7d712
Sebastian Margozzini <smargozzini1@gmail.com>
2021-10-19 11:06:05 -0300

    feat(customer_permission): change plan name to service and value

--- app/values/customer_permission.rb
+++ app/values/customer_permission.rb
@@ -3,13 +3,13 @@ class CustomerPermission
   include ActiveModel::Serialization
   attr_reader :current_usage, :limit, :allowed, :name
 
-  def initialize(permission, plan_permission, customer)
+  def initialize(permission, permission_group_permission, customer)
     @permission = permission
-    @plan_permission = plan_permission
+    @permission_group_permission = permission_group_permission
     @customer = customer
 
     @current_usage = customer_permission_service.usage_for_current_period
-    @limit = customer_permission_service.plan_permission_limit
+    @limit = customer_permission_service.permission_group_permission_limit
     @allowed = customer_permission_service.customer_allowed?
     @name = @permission.name
   end
@@ -19,7 +19,7 @@ class CustomerPermission
   def customer_permission_service
     @customer_permission_service ||= CustomerPermissionService.new(
       permission: @permission,
-      plan_permission: @plan_permission,
+      permission_group_permission: @permission_group_permission,
       customer: @customer
     )
   end
