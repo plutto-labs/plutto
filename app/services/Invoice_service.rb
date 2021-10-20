3821dc0def9e298b303be7a612253dd620f1f96d
Juan Ignacio Márquez <ignaciomarquezc@gmail.com>
2021-10-20 18:39:46 -0300

    refactor(invoice): move status change logic to Invoice service

--- app/services/Invoice_service.rb
+++ app/services/Invoice_service.rb
@@ -1,28 +1,51 @@
-class InvoiceService < PowerTypes::Service.new
-  def send_to_customer(invoice)
+class InvoiceService < PowerTypes::Service.new(:invoice)
+  def post!
     Analytics.track(
-      user_id: invoice.customer.organization_id,
+      user_id: @invoice.customer.organization_id,
       event: 'send invoice',
-      properties: invoice_properties(invoice)
+      properties: invoice_properties
     )
+    @invoice.update!(status: 'posted')
   end
 
-  def charge(invoice, payment_method)
-    kushki.charge(payment_method, invoice)
+  def charge!
+    if @invoice.customer.payment_methods.any?
+      begin
+        kushki.charge(@invoice.customer.payment_methods.first, @invoice)
+      rescue PluttoErrors::PaymentError => e
+        @invoice.update!(status: 'not_paid')
+        raise e
+      else
+        @invoice.update!(status: 'paid')
+      end
+    else
+      post!
+    end
   end
 
   private
 
-  def invoice_properties(invoice)
+  def post_if_configured
+    settings = customer.organization.settings
+    return unless settings
+
+    if settings['charge_invoices_automatically']
+      charge!
+    elsif settings['send_invoices_automatically']
+      post!
+    end
+  end
+
+  def invoice_properties
     {
-      invoice_date: invoice.issue_date,
-      invoice_total: { amount: invoice.subtotal.amount, currency: invoice.currency },
-      details: invoice.details,
-      customer_email: invoice.customer.email,
-      customer_name: invoice.customer.name,
-      customer_organization: invoice.customer.organization.name,
-      billing_information: invoice.customer.billing_information&.serializable_hash,
-      payment_link: kushki.enroll_link_for(invoice)
+      invoice_date: @invoice.issue_date,
+      invoice_total: { amount: @invoice.subtotal.amount, currency: @invoice.currency },
+      details: @invoice.details,
+      customer_email: @invoice.customer.email,
+      customer_name: @invoice.customer.name,
+      customer_organization: @invoice.customer.organization.name,
+      billing_information: @invoice.customer.billing_information&.serializable_hash,
+      payment_link: kushki.enroll_link_for(@invoice)
     }
   end
 
