4785814e2f4eadc79c44a7b1b7958066027ce5ce
Felipe Dominguez <fdominguezc@uc.cl>
2021-10-21 18:03:56 -0300

    chore(initializer/analytics): use Rails reloader

--- config/initializers/analytics.rb
+++ config/initializers/analytics.rb
@@ -1,9 +1,11 @@
 SEGMENT_WRITE_KEY =
   ENV.fetch('REPORT_ANALYTICS', false) == 'true' ? ENV.fetch('SEGMENT_RUBY_WRITE_KEY') : ''
 
-segment = Segment::Analytics.new(
-  write_key: SEGMENT_WRITE_KEY,
-  on_error: Proc.new { |_, msg| logger.error(msg) }
-)
+Rails.application.reloader.to_prepare do
+  segment = Segment::Analytics.new(
+    write_key: SEGMENT_WRITE_KEY,
+    on_error: Proc.new { |_, msg| logger.error(msg) }
+  )
 
-Analytics = SegmentService.new(segment: segment)
+  ::Analytics = SegmentService.new(segment: segment)
+end
