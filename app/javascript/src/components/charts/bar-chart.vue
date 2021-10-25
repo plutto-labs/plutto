cdc82a8ba20ac650f7a05c485b2b3fa1a44eec74
Felipe Dominguez <fdominguezc@uc.cl>
2021-10-25 18:52:05 -0300

    refactor(charts): make charts reactive and extract code

--- app/javascript/src/components/charts/bar-chart.vue
+++ app/javascript/src/components/charts/bar-chart.vue
@@ -5,8 +5,9 @@
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
+import { onMounted, watch } from 'vue';
 import Chart from 'chart.js/auto';
+import chartApi from './base-setup.js';
 
 export default {
   props: {
@@ -25,62 +26,20 @@ export default {
   },
 
   setup(props) {
-    const chartRef = ref(null);
-
-    const data = {
-      labels: Object.keys(props.datasets[Object.keys(props.datasets)[0]]),
-      datasets: [],
-    };
-
-    const config = {
-      type: 'bar',
-      data,
-      options: {
-        responsive: true,
-        plugins: {
-          legend: {
-            display: false,
-          },
-          title: {
-            display: true,
-            text: props.title,
-            color: '#a89a00',
-            padding: {
-              top: 10,
-              bottom: 15,
-            },
-          },
-          leyend: {
-            display: true,
-            position: 'top',
-            labels: {
-              color: 'rgb(255, 99, 132)',
-            },
-          },
-        },
-        scales: {
-          x: { stacked: true },
-          y: { stacked: true, beginAtZero: true },
-        },
-      },
-    };
+    const { chartRef, data, config } = chartApi().constants(props.datasets, 'bar', props.title);
 
     let chart;
+
+    watch(
+      () => props.datasets,
+      () => { chartApi().updateChartData(data, props, chart); },
+      { deep: true },
+    );
 
     onMounted(() => {
       const ctx = chartRef.value.getContext('2d');
       chart = new Chart(ctx, config);
-      Object.keys(props.datasets).forEach((key) => {
-        const dataset = {
-          label: key,
-          data: Object.values(props.datasets[key]),
-          backgroundColor:
-            ['#ffea00', '#dbc900', '#a89a00', '#756c00', '#423d00'][Object.keys(props.datasets).indexOf(key)],
-        };
-        data.datasets.push(dataset);
-      });
-      chart.data.datasets = data.datasets;
-      chart.update();
+      chartApi().updateChartData(data, props, chart);
     });
 
     return { chartRef };
--- app/javascript/src/components/charts/line-chart.vue
+++ app/javascript/src/components/charts/line-chart.vue
@@ -5,8 +5,9 @@
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
+import { onMounted, watch } from 'vue';
 import Chart from 'chart.js/auto';
+import chartApi from './base-setup.js';
 
 export default {
   props: {
@@ -25,76 +26,20 @@ export default {
   },
 
   setup(props) {
-    const chartRef = ref(null);
-
-    const data = {
-      labels: Object.keys(props.datasets[Object.keys(props.datasets)[0]]),
-      datasets: [
-        {
-          label: props.label,
-          data: props.data,
-          fill: true,
-          // pointBackgroundColor: '#a89a00',
-          tension: 0.1,
-          // borderColor: '#a89a00',
-        },
-      ],
-
-    };
-
-    const config = {
-      type: 'line',
-      data,
-      options: {
-        responsive: true,
-        spanGaps: true,
-        borderWidth: 1,
-        plugins: {
-          legend: {
-            display: false,
-          },
-          title: {
-            display: true,
-            text: props.title,
-            color: '#a89a00',
-            padding: {
-              top: 10,
-              bottom: 15,
-            },
-          },
-          leyend: {
-            display: true,
-            position: 'top',
-            labels: {
-              color: 'rgb(255, 99, 132)',
-            },
-          },
-        },
-        scales: {
-          y: { beginAtZero: true },
-        },
-      },
-    };
+    const { chartRef, data, config } = chartApi().constants(props.datasets, 'line', props.title);
 
     let chart;
+
+    watch(
+      () => props.datasets,
+      () => { chartApi().updateChartData(data, props, chart); },
+      { deep: true },
+    );
 
     onMounted(() => {
       const ctx = chartRef.value.getContext('2d');
       chart = new Chart(ctx, config);
-      Object.keys(props.datasets).forEach((key) => {
-        const dataset = {
-          label: key,
-          fill: true,
-          data: Object.values(props.datasets[key]),
-          borderColor:
-            ['#ffea00', '#dbc900', '#a89a00', '#756c00', '#423d00'][Object.keys(props.datasets).indexOf(key)],
-          pointBackgroundColor:
-            ['#ffea00', '#dbc900', '#a89a00', '#756c00', '#423d00'][Object.keys(props.datasets).indexOf(key)],
-        };
-        data.datasets.push(dataset);
-      });
-      chart.data.datasets = data.datasets;
-      chart.update();
+      chartApi().updateChartData(data, props, chart);
     });
 
     return { chartRef };
