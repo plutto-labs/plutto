b8b8672ef0536e90d1d393bd937ef8b63255b010
Juan Ignacio Márquez <ignaciomarquezc@gmail.com>
2021-10-28 17:21:22 -0300

    feat(customers): add navigation links to help users find customers

--- app/javascript/src/router/customers/inactive.vue
+++ app/javascript/src/router/customers/inactive.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
-    <PluttoTable
-      class="mt-6 rounded-lg"
-      :headers="headers"
-      :rows="customers"
-      :loading="loading"
-      @show-clicked="(customer) => $router.push({ name: 'customer', params: { id: customer.id } })"
-    />
+    <template v-if="customers && customers.length > 0">
+      <PluttoTable
+        class="mt-6 rounded-lg"
+        :headers="headers"
+        :rows="customers"
+        :loading="loading"
+        @show-clicked="(customer) => $router.push({ name: 'customer', params: { id: customer.id } })"
+      />
+    </template>
+    <div
+      class="absolute flex items-center justify-center w-full text-center h-1/2"
+      v-else
+    >
+      <div>
+        <p>
+          Ups! Seems like there's no <b>inactive</b> customers found.
+        </p>
+        <p>
+          Try looking for
+          <router-link
+            class="underline text-primary"
+            to="/customers/trial"
+          >
+            customers on trial.
+          </router-link>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
