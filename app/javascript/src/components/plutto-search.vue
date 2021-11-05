44c48a076016347c4862c0e9fd6144d9cf8a7f5a
Sebastian Margozzini <smargozzini1@gmail.com>
2021-11-05 09:52:38 -0300

    feat(search): create plutto search component

---
+++ app/javascript/src/components/plutto-search.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex items-center h-8 p-2 space-x-2 bg-gray-800 border border-gray-400 rounded shadow">
+    <span
+      class="w-6 text-2xl text-gray-400 plutto-icon"
+      :class="{ 'text-primary': newValuePresent }"
+    >{{ newValuePresent ? 'keyboard_return' : 'search' }}</span>
+    <input
+      type="text"
+      class="w-64 text-sm bg-gray-800 focus:outline-none"
+      v-model="searchString"
+      @keyup.enter="onEnter"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchString: '',
+      lastSearchedString: '',
+    };
+  },
+  methods: {
+    onEnter() {
+      this.lastSearchedString = this.searchString;
+      this.$emit('search', this.searchString);
+    },
+  },
+  computed: {
+    newValuePresent() {
+      return this.searchString !== this.lastSearchedString;
+    },
+  },
+};
+</script>
