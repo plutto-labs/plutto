5a805222272318db6e796bdd7ef6a0e0eeed0cb4
Sebastian Margozzini <smargozzini1@gmail.com>
2021-11-09 17:19:24 -0300

    feat(embed): add code box with example code

--- app/javascript/src/router/widget/embed.vue
+++ app/javascript/src/router/widget/embed.vue
@@ -1,9 +1,9 @@
 <template>
-  <main>
+  <main class="space-y-6">
     <h2 class="text-2xl">
       Design
     </h2>
-    <div class="flex flex-wrap gap-8 mt-4">
+    <div class="relative flex flex-wrap gap-8 z-1">
       <div
         v-for="(color, index) in colorInputs"
         :key="index"
@@ -16,14 +16,44 @@
         />
       </div>
     </div>
+    <h2 class="text-2xl">
+      Code
+    </h2>
+    <PluttoCopyableCode
+      class="code"
+      code-string="<div id='notifications-inbox' style='display: inline-block' />
+<script type='text/javascript'>
+  (function(i,s,o,g,r,a,m) {i['MagicBellObject'] = r;(i[r] =i[r] ||function() {
+  (i[r].q = i[r].q || []).push(arguments);}),(i[r].l = 1 * new Date());(a = s.createElement(o)), (
+  m = s.getElementsByTagName(o)[0]);a.async = 1;a.src = g;m.parentNode.insertBefore(a, m);
+  })(window,document,'script','https://assets.magicbell.io/magicbell.min.js','magicbell');
+
+  var target = document.getElementById('notifications-inbox');
+  var options = {
+    apiKey: '2f66fa2f2d6fd40fb0ea619ac12618a6957b4ce4',
+    userEmail: 'smargozzini1@gmail.com', // @TODO Replace with the logged in user's email
+    // You can use userExternalId instead of userEmail -  https://bit.ly/3oiDSAe
+    onNotificationClick: function(notification) {
+      if (notification.actionUrl) window.open(notification.actionUrl, '_self');
+    },
+    height: 500,
+    theme: {'icon':{'borderColor':'#6113A3','width':'24px'},'unseenBadge':{'backgroundColor':'#DF4759'},'header':{'backgroundColor':'#6113A3','textColor':'#ffffff','borderRadius':'16px'},'footer':{'backgroundColor':'#6113A3','textColor':'#ffffff','borderRadius':'16px'},'notification':{'default':{'textColor':'#15091F','borderRadius':'8px','backgroundColor':'#6113A3'},'unseen':{'backgroundColor':'#6113A3'},'unread':{'backgroundColor':'#6113A3'}}},
+  };
+
+  magicbell('render', target, options);
+</script>
+"
+    />
   </main>
 </template>
 <script>
 import PluttoColorInput from '@/components/widget/plutto-color-input';
+import PluttoCopyableCode from '@/components/widget/plutto-copyable-code';
 
 export default {
   components: {
     PluttoColorInput,
+    PluttoCopyableCode,
   },
   data() {
     return {
@@ -39,3 +69,9 @@ export default {
   },
 };
 </script>
+<style scoped>
+.code {
+  position: relative;
+  z-index: -1 !important;
+}
+</style>
