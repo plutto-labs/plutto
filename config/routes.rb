8988f129fdc4650f89834cfa7bea4aeff0f09523
Sebastian Margozzini <smargozzini1@gmail.com>
2021-11-10 17:16:09 -0300

    feat(permissions): add destroy endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
       resources :meters, only: [:index, :show, :update, :create, :destroy]
       resources :organizations, only: [:update]
       resources :payments, only: [:index, :show]
-      resources :permissions, only: [:index, :create]
+      resources :permissions, only: [:index, :create, :destroy]
       resources :permission_groups, only: [:index, :show, :create, :update, :destroy]
       resources :products, only: [:index, :show, :create, :update, :destroy] do
         resources :pricings, only: [:create, :update, :destroy]
