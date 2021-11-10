5693d8fcbe39243d34061cab9f9e00af265d4acc
Sebastian Margozzini <smargozzini1@gmail.com>
2021-11-10 17:19:26 -0300

    feat(slide-over): add slide over

---
+++ app/javascript/src/components/widget/plutto-slideover.vue
@@ -0,0 +1,58 @@
+<template>
+  <Transition name="slide-fade">
+    <div
+      v-if="showing"
+      class="fixed inset-0 z-10 flex items-center justify-end w-full h-screen bg-see-through"
+    >
+      <slot name="preview" />
+      <div class="relative right-0 flex w-full h-full max-w-3xl bg-white shadow-lg md:w-1/2">
+        <button
+          aria-label="close"
+          class="absolute top-0 right-0 m-6 text-xl"
+          @click.prevent="close"
+        >
+          <span class="w-6 h-6 fill-current text-temporary material-icons hover:opacity-60">close</span>
+        </button>
+        <div class="w-full max-h-screen overflow-x-hidden overflow-y-auto">
+          <slot name="content" />
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<script>
+export default {
+  props: {
+    showing: {
+      required: true,
+      type: Boolean,
+    },
+  },
+  methods: {
+    close() {
+      this.$emit('close');
+    },
+  },
+};
+</script>
+
+<style>
+.slide-fade-enter-active {
+  transition: all .3s;
+}
+
+.slide-fade-leave-active {
+  transition: all .3s cubic-bezier(1, .5, .8, 1);
+}
+
+.slide-fade-enter,
+.slide-fade-leave-to {
+  transform: translateX(1000px);
+  opacity: 0;
+}
+
+.bg-see-through {
+  background-color: rgba(0, 0, 0, .85);
+}
+</style>
