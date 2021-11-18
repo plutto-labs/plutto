37a6111025feebb84441ea350fe2b8fdd3f61d12
Sebastian Margozzini <smargozzini1@gmail.com>
2021-11-18 17:28:52 -0300

    feat(widget): add global component

--- app/javascript/packs/widget.js
+++ app/javascript/packs/widget.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { createI18n } from 'vue-i18n';
 
 import Widget from '@/widget.vue';
+import LandingWidget from '@/components/widget/landing-widget.vue';
 import router from '@/router/widget.js';
 import store from '@/store';
 
@@ -21,6 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
     el: '#vue-app',
     components: {
       Widget,
+      LandingWidget,
     },
   });
   app.config.globalProperties.environment = window.location.host.split('.')[0];
