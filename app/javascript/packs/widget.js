07678fa2b0e2a17c91e7a87bbc8cbb638528f4ae
Sebastian Margozzini <smargozzini1@gmail.com>
2021-11-19 16:33:38 -0300

    feat(landing): add carousel and missing links

--- app/javascript/packs/widget.js
+++ app/javascript/packs/widget.js
@@ -3,6 +3,7 @@ import { createI18n } from 'vue-i18n';
 
 import Widget from '@/widget.vue';
 import LandingWidget from '@/components/widget/landing-widget.vue';
+import LandingCarousel from '@/components/widget/landing-carousel.vue';
 import router from '@/router/widget.js';
 import store from '@/store';
 
@@ -23,6 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
     components: {
       Widget,
       LandingWidget,
+      LandingCarousel,
     },
   });
   app.config.globalProperties.environment = window.location.host.split('.')[0];
